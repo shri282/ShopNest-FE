c8ac7f0173bae0b2860ffc5612394d49f5035768
shrikadhir <shrikadhir.k@softsuave.org>
2025-06-10 17:50:16 +0530

    changes

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/register.css';
+import { User } from '../interfaces/User';
 
 const Register: React.FC = () => {
+    const [user, setUser] = useState<Partial<User>>({});
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value, type } = e.target;
+        const val = type === 'radio' ? e.target.value : value;
+        
+        setUser((prev) => {
+            return {
+                ...prev,
+                [name]: val
+            }
+        })
+    }
+
+    console.log(user);
+    
     return (
         <div className="register-container">
             <div className="register-form">
@@ -11,6 +27,7 @@ const Register: React.FC = () => {
                     <div className="form-group">
                         <label htmlFor="username">Username*</label>
                         <input
+                            onChange={handleChange}
                             type="text"
                             id="username"
                             name="username"
@@ -20,6 +37,7 @@ const Register: React.FC = () => {
                     <div className="form-group">
                         <label htmlFor="dob">Date of Birth</label>
                         <input
+                            onChange={handleChange}
                             type="date"
                             id="dob"
                             name="dob"
@@ -29,6 +47,7 @@ const Register: React.FC = () => {
                     <div className="form-group">
                         <label htmlFor="email">Email*</label>
                         <input
+                            onChange={handleChange}
                             type="email"
                             id="email"
                             name="email"
@@ -40,6 +59,7 @@ const Register: React.FC = () => {
                         <div className="radio-group">
                             <label>
                                 <input
+                                    onChange={handleChange}
                                     type="radio"
                                     name="gender"
                                     value="male"
@@ -48,6 +68,7 @@ const Register: React.FC = () => {
                             </label>
                             <label>
                                 <input
+                                    onChange={handleChange}
                                     type="radio"
                                     name="gender"
                                     value="female"
@@ -56,6 +77,7 @@ const Register: React.FC = () => {
                             </label>
                             <label>
                                 <input
+                                    onChange={handleChange}
                                     type="radio"
                                     name="gender"
                                     value="other"
@@ -68,6 +90,7 @@ const Register: React.FC = () => {
                     <div className="form-group">
                         <label htmlFor="phone">Phone Number</label>
                         <input
+                            onChange={handleChange}
                             type="tel"
                             id="phone"
                             name="phone"
@@ -80,6 +103,7 @@ const Register: React.FC = () => {
                     <div className="form-group">
                         <label htmlFor="password">Password*</label>
                         <input
+                            onChange={handleChange}
                             type="password"
                             id="password"
                             name="password"
@@ -89,6 +113,7 @@ const Register: React.FC = () => {
                     <div className="form-group">
                         <label htmlFor="confirmPassword">Confirm Password*</label>
                         <input
+                            onChange={handleChange}
                             type="password"
                             id="confirmPassword"
                             name="confirmPassword"
