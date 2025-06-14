a5907aeedd84e58614a2122032894a926cc37cec
shrikadhir <shrikadhir10@gmail.com>
2025-06-14 23:49:54 +0530

    data state for products

--- src/pages/user/UserDashboard.tsx
+++ src/pages/user/UserDashboard.tsx
@@ -6,20 +6,26 @@ import ProductCards from '../../components/ProductCards';
 import AdminSideDrawer from '../../components/admin/AdminSideDrawer';
 import Header from '../../components/Header';
 import ErrorSnackbar from '../../common/ErrorSnackBar';
+import DataState from '../../common/DataState';
 
 const UserDashboard: React.FC = () => {
 
   const [products, setProducts] = useState<IProduct[]>([]);
   const [error, setError] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
   const [errorPopupOpen, setErrorPopupOpen] = React.useState(false);
 
   const fetchProducts = useCallback(async () => {
+    setLoading(true);
+
     try {
       const data = await ProductService.getProducts();
       setProducts(data);
     } catch (error: any) {
       setError(error);
       setErrorPopupOpen(true);
+    } finally {
+      setLoading(false);
     }
   }, [])
 
@@ -30,7 +36,12 @@ const UserDashboard: React.FC = () => {
   return (
     <div className='user-dashboard'>
         <AdminSideDrawer Header={<Header />}>
-            <ProductCards products={products} />
+            <DataState
+              data={products}
+              error={error}
+              loading={loading}
+              render={(products) => <ProductCards products={products} />}
+            />
         </AdminSideDrawer>
         <ErrorSnackbar open={errorPopupOpen} message={error?.message} onClose={() => setErrorPopupOpen(false)} />
     </div>
