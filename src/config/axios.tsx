5f7dd853048dda12df753902de1ffba8d4eabbdb
shrikadhir <shrikadhir10@gmail.com>
2025-06-14 20:44:08 +0530

    changes

--- src/config/axios.tsx
+++ src/config/axios.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = process.env.BACKEND_URL ?? "http://localhost:8080";
+const BACKEND_URL = process.env.BACKEND_URL;
 
 export const apiPublic = axios.create({
     baseURL: BACKEND_URL,
